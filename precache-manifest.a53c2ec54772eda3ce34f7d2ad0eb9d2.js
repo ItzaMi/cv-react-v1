94cb6ffbf9b0cc63d349ae65cfd15ed7e0e0c377
ItzaMi <rros00@gmail.com>
2020-01-19 23:43:35 +0000

    Updates

---
+++ precache-manifest.a53c2ec54772eda3ce34f7d2ad0eb9d2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9446eef8927fad37ace993272879db9a",
+    "url": "/cv-react-v1/index.html"
+  },
+  {
+    "revision": "e30bfa2f15c8a427c2e0",
+    "url": "/cv-react-v1/static/css/main.63b2e6d4.chunk.css"
+  },
+  {
+    "revision": "f0a10381ff5f463576c1",
+    "url": "/cv-react-v1/static/js/2.4baf3a34.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/cv-react-v1/static/js/2.4baf3a34.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e30bfa2f15c8a427c2e0",
+    "url": "/cv-react-v1/static/js/main.7bf02c0e.chunk.js"
+  },
+  {
+    "revision": "a53110f9e84309db6046",
+    "url": "/cv-react-v1/static/js/runtime-main.730a0f41.js"
+  },
+  {
+    "revision": "200daf617054a80db7e465074b10489e",
+    "url": "/cv-react-v1/static/media/photo.200daf61.jpg"
+  }
+]);
